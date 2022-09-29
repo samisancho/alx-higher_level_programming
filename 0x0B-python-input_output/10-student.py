bce5c426f34acb97e5bde44958fcede043b82c4b
Samuel Daba <58066840+samisancho@users.noreply.github.com>
2022-09-29 09:32:13 +0300

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,26 +1,37 @@
 #!/usr/bin/python3
-"""
-Contains the clas "Student"
+"""Module 12-student.
+Creates a Student class.
 """
 
 
 class Student:
-    """Representation of a student"""
+    """Class that defines a student.
+    Public attributes:
+        - first_name
+        - last_name
+        - age
+    Public method to_json().
+    """
+
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        """Initializes the Student instance."""
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """returns a dictionary representation of a Student instance
-        with specified attributes"""
-        if attrs is None:
-            return self.__dict__
-        new_dict = {}
-        for a in attrs:
-            try:
-                new_dict[a] = self.__dict__[a]
-            except:
-                pass
-        return new_dict
+        """Retrieves a dictionary representation
+        of a Student instance.
+        Args:
+            - attrs: list of attributes
+        Returns: the dict representation of the instance.
+        """
+
+        my_dict = dict()
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            for x in attrs:
+                if x in self.__dict__:
+                    my_dict.update({x: self.__dict__[x]})
+            return my_dict
+        return self.__dict__.copy()
