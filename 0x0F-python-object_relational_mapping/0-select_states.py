e039b3ace3487af27faaad4f3e1d7522edfd953a
Samuel Daba <58066840+samisancho@users.noreply.github.com>
2023-02-28 16:14:50 +0300

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,35 @@
 #!/usr/bin/python3
-"""
-This script lists all states from the
-database `hbtn_0e_0_usa`.
-"""
-
+import sys
 import MySQLdb
-from sys import argv
 
-if __name__ == '__main__':
-    """
-    Access to the database and get the states
-    from the database.
-    """
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+"""
+    Script to conect to a sql db using MySQLdb client in python
+"""
 
+if len(sys.argv) != 4:
+    print("arguments must be 3")
+    sys.exit(1)
+else:
+    usrn = sys.argv[1]
+    passw = sys.argv[2]
+    db_name = sys.argv[3]
+    hos_t = "localhost"
+
+    db = MySQLdb.connect(host=hos_t, user=usrn,  passwd=passw, db=db_name)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
+
+    try:
+        cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+        rows = cur.fetchall()
+    except MySQLdb.Error:
+        str = "MySQL Error [{%d}]: {}"
+        try:
+            print(str.format(MySQLdb.Error.args[0], MySQLdb.Error.args[1]))
+        except:
+            print("MySQL ERROR: {}".format(str(MySQLdb.Error)))
 
     for row in rows:
         print(row)
+
+    cur.close()
+    db.close()
