662fc8f7cb5586424886c0c0615185aa31bcfc0b
Samuel Daba <58066840+samisancho@users.noreply.github.com>
2023-03-06 13:50:14 +0300

    Delete 0x11-python-network_1 directory

--- 0x11-python-network_1/5-hbtn_header.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import requests
-from sys import argv
-
-if __name__ == "__main__":
-    res = requests.get(argv[1])
-    x_request_id = res.headers.get('X-Request-Id')
-    print(x_request_id)
